122aa0eb2671cd20594091928a7a9233c1fedb76
bvarjavand <bvarjavand@gmail.com>
2019-05-11 21:46:14 -0400

    Updates

---
+++ precache-manifest.73a71dde7f75c2f69515fa60ac258faf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c1ba91634e20f3cbd8104ad3cbd88a",
+    "url": "/bvarjavand.com/index.html"
+  },
+  {
+    "revision": "84974d0ac5e6aa9aa7f6",
+    "url": "/bvarjavand.com/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/bvarjavand.com/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "84974d0ac5e6aa9aa7f6",
+    "url": "/bvarjavand.com/static/js/main.1f529771.chunk.js"
+  },
+  {
+    "revision": "68b83ddfaf567245d074",
+    "url": "/bvarjavand.com/static/js/runtime~main.3fa813d0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bvarjavand.com/static/media/logo.5d5d9eef.svg"
+  }
+]);
